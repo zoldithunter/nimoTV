f85aff801048450a0759f0f70f4661d6c148c103
Duy Hunter <zold.ithunter@gmail.com>
2020-08-10 23:04:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
+	$.get( 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSilSZT8gYAGJa0Z7vTlqdMgm0nv7slXARbp5kMht89t6kC1xxsgfUZmnKvLgWqpRzIyB-Z0K8QuNiC/pubhtml', function( html ) {
 
-	// use your spreadsheet id here
-	var SPREADSHEET_ID = '2PACX-1vSilSZT8gYAGJa0Z7vTlqdMgm0nv7slXARbp5kMht89t6kC1xxsgfUZmnKvLgWqpRzIyB-Z0K8QuNiC'
-	$.googleSheetToJSON(SPREADSHEET_ID)
-		.done(function(rows){
-			// each row is a row of data from the spreadsheet
-			console.log(rows);
-		})
-		.fail(function(err){
-			console.log('error!', err);
-		});
+    // Loop through elements you want to scrape content from
+    		$(html).find("table").find("tbody tr").each( function(){
+
+        var text = $(this).text();
+			console.log(text);
+        // Do something with content
+
+    } )
+
+} );
 });
