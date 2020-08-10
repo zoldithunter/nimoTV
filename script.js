877608c8bf045599ec1637c15c48b2ed22753db6
Duy Hunter <zold.ithunter@gmail.com>
2020-08-10 22:46:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $( function() {
-	readRuleFile("NimoTV_Luong.xlsx");
+	var SPREADSHEET_ID = '2PACX-1vSilSZT8gYAGJa0Z7vTlqdMgm0nv7slXARbp5kMht89t6kC1xxsgfUZmnKvLgWqpRzIyB-Z0K8QuNiC'
+	$.googleSheetToJSON(SPREADSHEET_ID)
+		.done(function(rows){
+			// each row is a row of data from the spreadsheet
+			console.log(rows);
+		})
+		.fail(function(err){
+			console.log('error!', err);
+		});
+	});
 });
 
 
