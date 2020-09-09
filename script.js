c3c78e87576a92585016ce2113bef895a5b04ec0
Duy Hunter <zold.ithunter@gmail.com>
2020-09-09 13:38:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ var data = [];
 $(document).ready(function(){
 	$.get( 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSilSZT8gYAGJa0Z7vTlqdMgm0nv7slXARbp5kMht89t6kC1xxsgfUZmnKvLgWqpRzIyB-Z0K8QuNiC/pubhtml', function( html ) {
 		$(html).find("table").find("tbody tr").each( function(index, item){
-			if (index < 13) {
+			if (index < 20) {
 				var $tds=$(this).find('td');
 				data.push({
-					stt:$tds.eq(0).text(),
-					hoten:$tds.eq(1).text(),
-					name:$tds.eq(2).text(),
-					nimoID:$tds.eq(3).text(),
-					cmt:$tds.eq(4).text(),
-					stk:$tds.eq(5).text(),
-					ctk:$tds.eq(6).text(),
-					nh:$tds.eq(7).text(),
-					numOfStone:$tds.eq(8).text(),
-					numOfAGCUp:$tds.eq(9).text(),
-					targetSalary:$tds.eq(10).text(),
-					sumSalary:$tds.eq(11).text(),
+					stt:$tds.eq(0).text().trim(),
+					hoten:$tds.eq(1).text().trim(),
+					name:$tds.eq(2).text().trim(),
+					nimoID:$tds.eq(3).text().trim(),
+					cmt:$tds.eq(4).text().trim(),
+					stk:$tds.eq(5).text().trim(),
+					ctk:$tds.eq(6).text().trim(),
+					nh:$tds.eq(7).text().trim(),
+					numOfStone:$tds.eq(8).text().trim(),
+					numOfAGCUp:$tds.eq(9).text().trim(),
+					targetSalary:$tds.eq(10).text().trim(),
+					sumSalary:$tds.eq(11).text().trim(),
 				})
 			}
 	    })
