eb02cf4a2cafe8b27a706e637213f413688f153e
Vu Van Duy <zold.ithunter@gmail.com>
2021-06-01 22:34:12 +0700

    as

--- script2.js
+++ script2.js
@@ -15,37 +15,19 @@ $(document).ready(function(){
                     obj[title[idx]] = $(this).text().trim();
                 })
                 data.push(obj);
-
-				// data.push({
-				// 	stt:$tds.eq(0).text().trim(),
-    //                 creator:$tds.eq(1).text().trim(),
-    //                 idPage:$tds.eq(2).text().trim(),
-    //                 mg:$tds.eq(3).text().trim(),
-    //                 numOfFollows:$tds.eq(4).text().trim(),
-    //                 liveStream:$tds.eq(5).text().trim(),
-    //                 video:$tds.eq(6).text().trim(),
-    //                 viewTB:$tds.eq(7).text().trim(),
-    //                 ccv:$tds.eq(8).text().trim(),
-    //                 hl:$tds.eq(9).text().trim(),
-    //                 tuongtac:$tds.eq(10).text().trim(),
-    //                 ytb:$tds.eq(11).text().trim(),
-    //                 tiktok:$tds.eq(12).text().trim(),
-				// })
 			}
 	    })
 	});
-    console.log(title);
-    console.log(data);
     $("#info").hide();
 
-	$("#submitCMT").click(function() {
+	$("#submit").click(function() {
         $("#data").empty();
         $("#info").hide();
         if ($("#idPage").val() == "") {
             alert("Không có thông tin");
         } else {
             var filter = {
-                    idPage:$("#idPage").val()
+                    'ID Fanpage' : $("#idPage").val()
             }
             var rs = data.filter(function(item){
                             for (var key in filter) {
@@ -57,64 +39,7 @@ $(document).ready(function(){
             if (rs.length == 0) {
                 alert("Thông tin ID Fanpage: " + $("#idPage").val() + " sai");
             } else {
-
-            }
-        }
-
-
-
-
-
-        if ($("#nimoID").val() == "" && $("#cmt").val() == "") {
-            alert("Không có thông tin");
-        } else {
-            if ($("#nimoID").val() != "" && $("#cmt").val() != "") {
-                var filter = {
-                    nimoID:$("#nimoID").val(),
-                    cmt:$("#cmt").val(),
-                }
-                var rs = data.filter(function(item){
-                            for (var key in filter) {
-                                if (item[key] === undefined || item[key] != filter[key])
-                                    return false;
-                            }
-                            return true;
-                        });
-                if (rs.length == 0) {
-                    alert("Thông tin nimoID: " + $("#nimoID").val() + " và cmt: " + $("#cmt").val() + " sai");
-                } else {
-                    $("#infoNimo").show();
-                    $("#stone").text(rs[0].numOfStone);
-                    $("#stoneAGC").text(rs[0].numOfAGCUp);
-                    $("#target").text(rs[0].targetSalary);
-                    $("#info").show();
-                    $("#hoten").text(rs[0].hoten);
-                    $("#stk").text(rs[0].stk);
-                    $("#ctk").text(rs[0].ctk);
-                    $("#nh").text(rs[0].nh);
-                    $("#salary").text(rs[0].sumSalary);
-                }
-            } else if ($("#nimoID").val() != "" && $("#cmt").val() == "") {
-                var tmp = data.find(x => x.nimoID === $("#nimoID").val());
-                if (tmp === undefined) {
-                    alert("Không tồn tại niMoID: " + $("#nimoID").val());
-                } else {
-                    $("#infoNimo").show();
-                    $("#stone").text(tmp.numOfStone);
-                    $("#stoneAGC").text(tmp.numOfAGCUp);
-                    $("#target").text(tmp.targetSalary);
-                }
-            } else if ($("#nimoID").val() == "" && $("#cmt").val() != "") {
-                var tmp = data.find(x => x.cmt === $("#cmt").val());
-                if (tmp === undefined) {
-                    alert("Không tồn tại CMT: " + $("#cmt").val());
-                } else {
-                    $("#info").show();
-                    $("#hoten").text(tmp.hoten);
-                    $("#stk").text(tmp.stk);
-                    $("#ctk").text(tmp.ctk);
-                    $("#nh").text(tmp.nh);
-                }
+                console.log(rs);
             }
         }
 	});
